39cae759b003c4805335b7b917e742da3f83469a
大漠知秋 <qsguo@car2p.com>
2022-12-05 15:24:55 +0800

    Init

---
+++ user/src/main/java/com/lynchj/study/user/controller/ShareController.java
@@ -0,0 +1,73 @@
+package com.lynchj.study.user.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ *                                        ,s555SB@@&
+ *                                      :9H####@@@@@Xi
+ *                                     1@@@@@@@@@@@@@@8
+ *                                   ,8@@@@@@@@@B@@@@@@8
+ *                                  :B@@@@X3hi8Bs;B@@@@@Ah,
+ *             ,8i                  r@@@B:     1S ,M@@@@@@#8;
+ *            1AB35.i:               X@@8 .   SGhr ,A@@@@@@@@S
+ *            1@h31MX8                18Hhh3i .i3r ,A@@@@@@@@@5
+ *            ;@&i,58r5                 rGSS:     :B@@@@@@@@@@A
+ *             1#i  . 9i                 hX.  .: .5@@@@@@@@@@@1
+ *              sG1,  ,G53s.              9#Xi;hS5 3B@@@@@@@B1
+ *               .h8h.,A@@@MXSs,           #@H1:    3ssSSX@1
+ *               s ,@@@@@@@@@@@@Xhi,       r#@@X1s9M8    .GA981
+ *               ,. rS8H#@@@@@@@@@@#HG51;.  .h31i;9@r    .8@@@@BS;i;
+ *                .19AXXXAB@@@@@@@@@@@@@@#MHXG893hrX#XGGXM@@@@@@@@@@MS
+ *                s@@MM@@@hsX#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&,
+ *              :GB@#3G@@Brs ,1GM@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@B,
+ *            .hM@@@#@@#MX 51  r;iSGAM@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@8
+ *          :3B@@@@@@@@@@@&9@h :Gs   .;sSXH@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@:
+ *      s&HA#@@@@@@@@@@@@@@M89A;.8S.       ,r3@@@@@@@@@@@@@@@@@@@@@@@@@@@r
+ *   ,13B@@@@@@@@@@@@@@@@@@@5 5B3 ;.         ;@@@@@@@@@@@@@@@@@@@@@@@@@@@i
+ *  5#@@#&@@@@@@@@@@@@@@@@@@9  .39:          ;@@@@@@@@@@@@@@@@@@@@@@@@@@@;
+ *  9@@@X:MM@@@@@@@@@@@@@@@#;    ;31.         H@@@@@@@@@@@@@@@@@@@@@@@@@@:
+ *   SH#@B9.rM@@@@@@@@@@@@@B       :.         3@@@@@@@@@@@@@@@@@@@@@@@@@@5
+ *     ,:.   9@@@@@@@@@@@#HB5                 .M@@@@@@@@@@@@@@@@@@@@@@@@@B
+ *           ,ssirhSM@&1;i19911i,.             s@@@@@@@@@@@@@@@@@@@@@@@@@@S
+ *              ,,,rHAri1h1rh&@#353Sh:          8@@@@@@@@@@@@@@@@@@@@@@@@@#:
+ *            .A3hH@#5S553&@@#h   i:i9S          #@@@@@@@@@@@@@@@@@@@@@@@@@A.
+ *
+ * 通过服务发现调用其他项目的测试相关接口
+ *
+ * @author 大漠知秋
+ * @date 2022/12/5 13:00
+ */
+@RestController
+@RequestMapping(value = "/share")
+public class ShareController {
+
+    @Resource
+    private RestTemplate    restTemplate;
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/getOrder/{id}")
+    public String getOrder(@PathVariable("id") Long id) {
+        // 从服务发现中获取 order 对应的服务实例列表
+        List<ServiceInstance> orders = discoveryClient.getInstances("order");
+        // 随机获取一个
+        ServiceInstance instance = orders.get(ThreadLocalRandom.current().nextInt(orders.size()));
+        // 拼上访问地址
+        String url = instance.getUri() + "/order/getOrder/{id}";
+
+        // 发起访问
+        String resp = restTemplate.getForObject(url, String.class, id);
+        return "Share " + resp;
+    }
+
+}
