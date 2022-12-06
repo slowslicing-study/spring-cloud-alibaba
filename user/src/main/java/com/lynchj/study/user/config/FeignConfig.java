1cec4a8cb536b731c9c8c1139d4f0931a72747dc
大漠知秋 <qsguo@car2p.com>
2022-12-06 21:25:04 +0800

    整理代码

--- user/src/main/java/com/lynchj/study/user/config/FeignConfig.java
+++ user/src/main/java/com/lynchj/study/user/config/FeignConfig.java
@@ -2,12 +2,11 @@ package com.lynchj.study.user.config;
 
 import feign.Logger;
 import feign.Request;
-import org.springframework.context.annotation.Bean;
 
 //@Configuration // 如果加上此注解，则全局生效，否则就只生效在配置的 Feign 上
 public class FeignConfig {
 
-    @Bean
+//    @Bean
     public Logger.Level level() {
         return Logger.Level.FULL;
     }
--- user/src/main/java/com/lynchj/study/user/feign/ShareFeign.java
+++ user/src/main/java/com/lynchj/study/user/feign/ShareFeign.java
@@ -1,12 +1,11 @@
 package com.lynchj.study.user.feign;
 
-import com.lynchj.study.user.config.FeignConfig;
 import com.lynchj.study.user.model.Order;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.cloud.openfeign.SpringQueryMap;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "order", configuration = FeignConfig.class)
+@FeignClient(name = "order")
 public interface ShareFeign {
 
     @GetMapping("/order/getOrder/{id}")
