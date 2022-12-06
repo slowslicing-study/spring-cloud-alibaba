23a2933d3517b43cb6c4fe8e8327a786a05704ae
大漠知秋 <qsguo@car2p.com>
2022-12-06 21:18:08 +0800

    配置 Feign 重试

--- user/src/main/java/com/lynchj/study/user/config/FeignConfig.java
+++ user/src/main/java/com/lynchj/study/user/config/FeignConfig.java
@@ -17,4 +17,13 @@ public class FeignConfig {
         return new Request.Options(2000, 2000);
     }
 
+    /*@Bean
+    public Retryer retryer() {
+        // period=1000 当前请求失败之后 到 发起下一次重试请求之间的间隔时间，单位毫秒
+        // maxPeriod=1000 发起当前请求的最大间隔时间，单位毫秒
+        // maxAttempts=2 重试次数，包括第一次请求，所以如果想要重试 2 次，就需要设置为 3
+        Retryer retryer = new Retryer.Default(1000, 1000, 3);
+        return retryer;
+    }*/
+
 }
